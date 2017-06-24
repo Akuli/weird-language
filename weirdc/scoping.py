7fa3799671900de394f8d5b624674543a959b64b
Purple Myst <PurpleMyst@users.noreply.github.com>
2017-06-24 18:48:13 +0200

    Refactor scoping.py

--- weirdc/scoping.py
+++ weirdc/scoping.py
@@ -2,7 +2,7 @@
 import collections
 import random
 
-from . import ast, c_output
+from . import ast
 
 
 def creates_scope(node, *, classes=(ast.FunctionDef, ast.If)):
@@ -13,51 +13,33 @@ def is_literal(node, *, classes=(ast.String, ast.Integer)):
     return isinstance(node, classes)
 
 
-def is_weird_object(type_):
-    if isinstance(type_, ast.Name):
-        type_ = type_.name
-    return type_ in c_output.OBJECTS
-
-
-def modify_body(node, new_body):
-    node.body[:] = new_body
-    return node
-
-
-def scope_node(scopes, node):
-    simplified_body = scope_ast(node.body, scopes)
-    return modify_body(node, simplified_body)
-
-
-def scope_ast(nodes, scopes=None, return_types={}):
+def scope_ast(nodes, scopes=None, return_types=None):
     if scopes is None:
         # We start off with a global scope.
         scopes = collections.ChainMap()
 
+    if return_types is None:
+        return_types = {}
+
     scoped_nodes = []
     returned_names = {}
-    index = 0
 
-    def _actually_store_literal(value, type_):
-        nonlocal index  # I FINALLY FOUND A USE FOR THIS!
-
+    def _create_variable(value, var_type):
         rand = "".join(filter(str.isdigit, str(random.random())))
         variable = "literal" + rand
 
         assert variable not in scopes
-        scopes[variable] = type_
+        scopes[variable] = var_type
 
-        scoped_nodes.insert(
-            index, ast.Declaration(None, None, type_, variable, value)
+        scoped_nodes.append(
+            ast.Declaration(None, None, var_type, variable, value)
         )
-
-        # We must offset all further indexes by one.
-        index += 1
 
         return ast.Name(None, None, variable)
 
-    def _store_literals(value, *, from_return=False):
+    def _store_literals(value):
         if isinstance(value, ast.ExpressionStatement):
+            # TODO: Save the whole expression's result to a temporary var.
             value.expression = _store_literals(value.expression)
             return value
         elif isinstance(value, ast.FunctionCall):
@@ -65,24 +47,24 @@ def scope_ast(nodes, scopes=None, return_types={}):
                 value.arguments[j] = _store_literals(arg)
 
             if value.function.name in return_types:
-                return _actually_store_literal(value,
-                                               return_types[value.function.name])
-            else:
-                # TODO: This is a builtin, and we must someway know their return
-                # types.
-                return value
+                return _create_variable(
+                    value, return_types[value.function.name])
+            # XXX: This return statement is only executed when the function is
+            # a builtin, ergo it's not in return_types. Maybe we could
+            # circumvent this by providing return_types with the builtins'
+            # types?
+            return value
         elif isinstance(value, ast.Return):
-            value.value = _store_literals(value.value, from_return=True)
+            value.value = _store_literals(value.value)
+            returned_names[value.value.name] = len(scoped_nodes)
             return value
         elif not is_literal(value):
             return value
-        # XXX: Is there anything to add?
 
-        name = _actually_store_literal(
+        name = _create_variable(
             value, ast.Name(None, None, value.__class__.__name__)
         )
-        if from_return:
-            returned_names[name.name] = index
+
         return name
 
     for node in nodes:
@@ -93,30 +75,27 @@ def scope_ast(nodes, scopes=None, return_types={}):
             else:
                 scopes[variable] = node.type
         elif isinstance(node, ast.Return) and isinstance(node.value, ast.Name):
-            returned_names[node.value.name] = index
+            # We have to account for the fact that the return statement is
+            # added to scoped_nodes later.
+            returned_names[node.value.name] = len(scoped_nodes) + 1
         elif isinstance(node, ast.FunctionDef):
             return_types[node.name] = node.returntype
 
         node = _store_literals(node)
         if creates_scope(node):
-            simplified_node = scope_node(scopes.new_child(), node)
-            scoped_nodes.append(simplified_node)
-        else:
-            scoped_nodes.append(node)
+            node.body[:] = scope_ast(node.body, scopes.new_child())
+        scoped_nodes.append(node)
 
-        index += 1
-
-    decrefs = []
-    for name, type_ in scopes.maps[0].items():
-        if name not in returned_names and is_weird_object(type_):
-            decrefs.append(ast.DecRef(name))
+    decrefs = [ast.DecRef(name)
+               for name in scopes.maps[0]
+               if name not in returned_names]
 
     # XXX: Figure out why this works.
     if returned_names:
-        for j in set(returned_names.values()) | {index - 1}:
+        for j in set(returned_names.values()) | {len(scoped_nodes) - 1}:
             scoped_nodes[j:j] = decrefs
     else:
-        scoped_nodes[index:index] = decrefs
+        scoped_nodes.extend(decrefs)
 
     return scoped_nodes
 
